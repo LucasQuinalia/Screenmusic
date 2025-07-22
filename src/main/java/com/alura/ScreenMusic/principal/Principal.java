5f370e5fdd06305dfe639b11619e96a596865aa6
Lucas Quinália <lrgquinalia@gmail.com>
2025-07-22 15:16:55 -0300

    Implementação de cadastro de artista

--- src/main/java/com/alura/ScreenMusic/principal/Principal.java
+++ src/main/java/com/alura/ScreenMusic/principal/Principal.java
@@ -1,5 +1,7 @@
 package com.alura.ScreenMusic.principal;
 
+import com.alura.ScreenMusic.model.Artista;
+import com.alura.ScreenMusic.model.TipoArtista;
 import com.alura.ScreenMusic.repository.ArtistaRepository;
 
 import java.util.Scanner;
@@ -15,7 +17,7 @@ public class Principal {
 
     public void exibeMenu() {
         var opcao = -1;
-        while (opcao != 0) {
+        while (opcao != 9) {
             var menu = """
                     *** ScreenMusic ***
                     
@@ -48,12 +50,25 @@ public class Principal {
                     buscarArtista();
                 default:
                     System.out.println("Opção inválida!");
-
             }
         }
     }
 
     private void cadastrarArtista() {
+        var continuar = "s";
+
+        while (continuar.toLowerCase().equals("s")) {
+            System.out.println("Digite o nome do artista que deseja cadastrar:");
+            var nome = leitura.nextLine();
+            System.out.println("Digite o tipo do artista (solo, dupla ou banda):");
+            var tipo = leitura.nextLine();
+            TipoArtista tipoArtista = TipoArtista.valueOf(tipo.toUpperCase());
+            Artista artista = new Artista(nome, tipoArtista);
+            repositorio.save(artista);
+
+            System.out.println("Deseja cadastrar mais um artista? (S/N)");
+            continuar = leitura.nextLine();
+        }
     }
 
     private void cadastrarMusica() {
@@ -69,6 +84,14 @@ public class Principal {
     }
 
     private void buscarArtista() {
-        
+        System.out.println("Qual o nome do artista que deseja buscar?");
+        var nome = leitura.nextLine();
+        var artistaBuscado = repositorio.findByNomeContainingIgnoreCase(nome);
+
+        if (artistaBuscado.isPresent()) {
+            System.out.println("Dados do artista encontrado: " + artistaBuscado.get());
+        } else {
+            System.out.println("Artista não encontrado!");
+        }
     }
 }
