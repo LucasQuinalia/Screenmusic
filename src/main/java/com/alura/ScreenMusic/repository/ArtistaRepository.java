1dca7ce90eefadcd8b98ef9651eee7e3b4efc159
Lucas Quinália <lrgquinalia@gmail.com>
2025-07-22 15:17:10 -0300

    Implementação de busca de artista

--- src/main/java/com/alura/ScreenMusic/repository/ArtistaRepository.java
+++ src/main/java/com/alura/ScreenMusic/repository/ArtistaRepository.java
@@ -1,4 +1,10 @@
 package com.alura.ScreenMusic.repository;
 
-public class ArtistaRepository {
+import com.alura.ScreenMusic.model.Artista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+    Optional<Artista> findByNomeContainingIgnoreCase(String nomeArtista);
 }
